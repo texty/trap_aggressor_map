4bb459f4499c8fabfc861fc32a40df350720711a
Nikko Mwordlling <nikkomwordlling@gmail.com>
2025-10-07 11:54:52 +0300

    2025 update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -27,5 +27,8 @@ export type LangType = 'uk' | 'en';
 export const asset_type_dict: Record<string, Record<LangType, string>> = {
 	apartment: { uk: 'Квартира', en: 'Apartment' },
 	house: { uk: 'Будинок', en: 'House' },
-	company: { uk: 'Компанія', en: 'Company' }
+	company: { uk: 'Компанія', en: 'Company' },
+	patent: { uk: 'Патент', en: 'Patent' },
+	land: { uk: 'Ділянка', en: 'Landscape' },
+	yacht: { uk: 'Яхта', en: 'Yacht' }
 };
